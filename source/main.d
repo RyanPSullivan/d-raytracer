e8436c2924eed5967a9ee9c97d3063f2c228e1ec
Ryan Sullivan <ryan@bossalien.com>
2014-10-17 14:31:30 +0100

    more updates to algorithm

--- source/main.d
+++ source/main.d
@@ -2,7 +2,8 @@
    hello world message
    to the console.  */
 import std.stdio;
- 
+import generators;
+
 int calculateRayDirection( int x, int y )
 {
   return 0;
@@ -13,35 +14,88 @@ bool intersect(int object, int ray)
   return true;
 }
 
-int getClosestObject(int ray)
+struct Vector
+{
+}
+
+struct Object
+{
+}
+
+struct Collision
+{
+  Object object;
+  Vector location;
+  Vector normal;
+}
+
+int getClosestCollidingObject(int ray)
 {
   auto objects = [1,2,3];
+  
+  Collision returningCollision;
+
+  auto minDistance = int.max;
 
   foreach(object; objects)
     {
-      if(intersect(object, ray))
+      auto collision = intersect( object, ray );
+      auto distance = distance( eyePosition, collision.location);
+      
+      if( distance < minDistance )
 	{
-	  return 1;
+	  returningCollision = collision;
+	  minDistance = distance;
 	}
     }
 
-  return -1;
+  return returningCollision;
 }
 
+struc Pixel
+{
+  byte[] red;
+  byte[] green;
+  byte[] blue;
+  byte[] alpha;
+}
+
+
+Pixel generatePixel( int x, int y )
+{
+  auto ray = calculateRayDirection(x,y);
+	  
+  auto object = getClosestObject(ray);
+
+  //if the ray collided with an object
+  if( object != NULL )
+    {
+      //compute illimunation
+    }   
+}
+
+void generateBitmap( string path, int imageWidth, int imageHeight )
+{
+  //write header
+
+  //write bitmap data
+  foreach(x; 0..imageWidth)
+    {
+      foreach(y; 0..imageHeight)
+	{
+	  auto pixel = generatePixel( x, y );
+	}
+    }
+}
 
 void main()
 {
   auto imageWidth = 100;
   auto imageHeight = 100;
 
-  foreach(x; 0 .. imageWidth)
-    {
-      foreach(y; 0 .. imageHeight)
-	{
-	  auto ray = calculateRayDirection(x,y);
-	  
-	  auto object = getClosestObject(ray);
-	}
-    }
+  foreach( pixel; generator( &generatePixel, imageWidth, imageHeight)
+	   {
+	     writeln(pixel);
+	   }
   writeln("Hello, World!");
 }
