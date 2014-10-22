3e05997d6f541e515b9bbbcca1de9e533c305796
Ryan Sullivan <ryan@bossalien.com>
2014-10-22 14:11:52 +0100

    more implementation

--- source/main.d
+++ source/main.d
@@ -20,6 +20,11 @@ struct Vector
 
 struct Object
 {
+  @property bool isTransparent { return m_Transparent; }
+
+private:
+
+  bool m_Transparent;
 }
 
 struct Collision
@@ -29,7 +34,7 @@ struct Collision
   Vector normal;
 }
 
-int getClosestCollidingObject(int ray)
+Collision getClosestCollision(int ray)
 {
   auto objects = [1,2,3];
   
@@ -52,26 +57,54 @@ int getClosestCollidingObject(int ray)
   return returningCollision;
 }
 
-struc Pixel
+struct Colour
 {
-  byte[] red;
-  byte[] green;
-  byte[] blue;
-  byte[] alpha;
+  Colour( byte r, byte g, byte b, byte a ) : red(r), green(g), blue(b), alpha(a) {}
+  static const Colour BlACK = Colour( 0, 0, 0, 0 );
+
+  byte red;
+  byte green;
+  byte blue;
+  byte alpha;
 }
 
 
+Colour trace(Vector ray, int depth)
+{
+  auto collision = getClosestCollision( ray );
+  
+  //no collision occured return a black pixel for now
+  if( collision == null )
+    {
+      return Pixel.BLACK;
+    }
+
+  Colour reflectionColour;
+  Colour refractionColour;
+
+  //ray collided with an object
+  if( collision.object.IsReflective )
+    {
+      auto reflectionRay = computeReflectionRay( ray, collision.location );
+
+      reflectionColour = trace( reflectionRay, depth + 1 );
+    }
+
+  if( collision.object.IsTransparent )
+  {
+    auto refractiveRay = computeRefractiveRay();
+    
+    refractionColour = trace( refractiveRay, depth + 1 );
+  }
+
+
+
+}
 Pixel generatePixel( int x, int y )
 {
   auto ray = calculateRayDirection(x,y);
-	  
-  auto object = getClosestObject(ray);
 
-  //if the ray collided with an object
-  if( object != NULL )
-    {
-      //compute illimunation
-    }   
+  return trace( ray, 0 ); 
 }
 
 void generateBitmap( string path, int imageWidth, int imageHeight )
@@ -86,6 +119,8 @@ void generateBitmap( string path, int imageWidth, int imageHeight )
 	  auto pixel = generatePixel( x, y );
 	}
     }
+
+  //write footer
 }
 
 void main()
@@ -93,9 +128,6 @@ void main()
   auto imageWidth = 100;
   auto imageHeight = 100;
 
-  foreach( pixel; generator( &generatePixel, imageWidth, imageHeight)
-	   {
-	     writeln(pixel);
-	   }
+  generateBitmap("output.bmp", imageWidth, imageHeight);
   writeln("Hello, World!");
 }
